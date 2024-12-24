cb9ea372f718df3766e69ba1838cd8e0d147a334
SOHAIB DAOUDI <sohaib.daoudi.9404@gmail.com>
2024-12-24 16:04:12 +0100

    Add files via upload

---
+++ forgot-password.php
@@ -0,0 +1,48 @@
+<?php require_once "controllerUserData.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Forgot Password</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form">
+                <form action="forgot-password.php" method="POST" autocomplete="off">
+                    <h2 class="text-center">Forgot Password</h2>
+                    <p class="text-center text">Please enter your email address and your backup code to update your password</p>
+                    <?php
+                    // Display errors if any
+                    if (count($errors) > 0) {
+                        ?>
+                        <div class="alert alert-danger text-center">
+                            <?php 
+                            foreach ($errors as $error) {
+                                echo $error;
+                            }
+                            ?>
+                        </div>
+                        <?php
+                    }
+                    ?>
+
+                    <div class="form-group">
+                        <input class="form-control" type="email" name="email" placeholder="Enter your email address" required>
+                    </div>
+
+                    <div class="form-group">
+                        <input class="form-control" type="text" name="code" placeholder="Enter backup code" required>
+                    </div>
+
+                    <div class="form-group">
+                        <input class="form-control button" type="submit" name="check-code" value="Continue">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ new-password.php
@@ -0,0 +1,51 @@
+<?php require_once "controllerUserData.php"; ?>
+<?php 
+$email = $_SESSION['email'];
+if($email == false){
+  header('Location: login-user.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create a New Password</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form">
+                <form action="new-password.php" method="POST" autocomplete="off">
+                    <h2 class="text-center">New Password</h2>
+                    <p class="text-center text">Update your password</p>
+                    <?php
+                    if(count($errors) > 0){
+                        ?>
+                        <div class="alert alert-danger text-center">
+                            <?php
+                            foreach($errors as $showerror){
+                                echo $showerror;
+                            }
+                            ?>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                    <div class="form-group">
+                        <input class="form-control" type="password" name="password" placeholder="Create new password" required>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="password" name="cpassword" placeholder="Confirm your password" required>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control button" type="submit" name="change-password" value="Change">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
