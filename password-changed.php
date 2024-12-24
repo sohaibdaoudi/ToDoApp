e7eaf2beaa067822f495c9567b0904f5d29ed276
Majidi0 <majidi.marouane0@gmail.com>
2024-12-24 21:07:56 +0100

    Update password-changed.php

--- password-changed.php
+++ password-changed.php
@@ -8,31 +8,47 @@ if($_SESSION['info'] == false){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 offset-md-4 form login-form">
-            <?php 
-            if(isset($_SESSION['info'])){
-                ?>
-                <div class="alert alert-success text-center">
-                <?php echo $_SESSION['info']; ?>
-                </div>
-                <?php
+    <style>
+        @media (max-width: 576px) {
+            body,html{
+                background-color:white;
             }
-            ?>
-                <form action="login-user.php" method="POST">
-                    <div class="form-group">
-                        <input class="form-control button" type="submit" name="login-now" value="Login Now">
+
+            div.shadow-sm{
+                box-shadow:none!important;
+            }
+
+        }
+    </style>
+</head>
+<body class="bg-light">
+    <div class="container form-container">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-12 col-sm-10 col-md-8 col-lg-6">
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        <?php 
+                        if(isset($_SESSION['info'])){
+                            ?>
+                            <div class="alert alert-success text-center">
+                                <?php echo $_SESSION['info']; ?>
+                            </div>
+                            <?php
+                        }
+                        ?>
+                        <form action="login-user.php" method="POST">
+                            <div class="form-group">
+                                <input class="btn btn-primary btn-block" type="submit" name="login-now" value="Login Now">
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     </div>
-    
 </body>
 </html>
